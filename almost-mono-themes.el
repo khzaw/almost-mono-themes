2c966cb63adc8f0cca393edc7e186111086279db
Kaung Htet <kayhzaw@gmail.com>
2025-05-09 14:20:32 +0800

    Do not underline font-lock-warning-face

--- almost-mono-themes.el
+++ almost-mono-themes.el
@@ -126,7 +126,7 @@
       (font-lock-keyword-face (:bold t))
       (font-lock-function-name-face (:bold t))
       (font-lock-variable-name-face (:foreground ,foreground))
-      (font-lock-warning-face (:foreground ,foreground :underline (:color ,warning :style line)))
+      (font-lock-warning-face (:foreground ,foreground))
       (font-lock-builtin-face (:bold t))
       ;; (font-lock-property-use-face (:bold t))
       (font-lock-constant-face (:foreground ,foreground))
